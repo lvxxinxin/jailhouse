7caeaf05cfaf7a1bf3caa5f08dcb9c37d5307757
Nikhil Devshatwar <nikhil.nd@ti.com>
2017-07-20 14:49:31 +0530

    hypervisor: Fix IROUTER register access
    Correct IROUTER base address
    Using void pointer for IROUTER register address
    Correct pointer arithmatic considering 32bit register per IRQ
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- hypervisor/arch/arm-common/include/asm/gic.h
+++ hypervisor/arch/arm-common/include/asm/gic.h
@@ -38,7 +38,7 @@
 #define GICD_SGIR			0x0f00
 #define GICD_CPENDSGIR			0x0f10
 #define GICD_SPENDSGIR			0x0f20
-#define GICD_IROUTER			0x6000
+#define GICD_IROUTER			0x6100
 
 #define GICD_PIDR2_ARCH(pidr)		(((pidr) & 0xf0) >> 4)
 
